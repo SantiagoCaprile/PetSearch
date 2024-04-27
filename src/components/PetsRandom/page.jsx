a8f8c2f3de32e131346d29638d7d94833867480c
Santi Caprile <santiagocaprile7@gmail.com>
2024-04-26 21:04:36 -0300

    Update Homepage and PetsRandom component styles for mobile devices

--- src/components/PetsRandom/page.jsx
+++ src/components/PetsRandom/page.jsx
@@ -3,7 +3,7 @@ import { pets } from "../../utils/petListTest";
 
 export default function PetsRandom() {
   return (
-    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 mx-2">
+    <div className="mt-6 grid md:grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 mx-2">
       {pets.slice(0, 4).map((pet) => (
         <Pet key={pet.id} pet={pet} />
       ))}
