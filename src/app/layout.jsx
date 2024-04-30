8018c5603b6e204d3d0a9c48c0bb6fcf9ae639ec
Santi Caprile <santiagocaprile7@gmail.com>
2024-04-30 02:51:22 -0300

    Fix and Update layout.jsx to use min-h-lvh instead of min-h-screen for body className because it was deprecated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     <SessionProvider>
       <Provider store={store}>
         <html lang="en">
-          <body className={inter.className + " flex flex-col min-h-screen"}>
+          <body className={inter.className + " flex flex-col min-h-lvh"}>
             <Nav />
             <main className="flex flex-col flex-1 bg-[#EFEEF1]">{children}</main>
             <footer className="flex flex-col  justify-evenly mt-auto bg-slate-700">
