25ae70463f9fcebda7feebd7545f57062dafaa03
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-15 01:46:24 -0300

    refactor: Update Carousel page to add max height to image container WIP

--- src/components/Carousel/page.jsx
+++ src/components/Carousel/page.jsx
@@ -21,9 +21,9 @@ const EmblaCarousel = (props) => {
     return (
         <section className="embla flex flex-col justify-center items-center">
             <div className="embla__viewport" ref={emblaRef}>
-                <div className="embla__container">
+                <div className="embla__container max-h-[400px]">
                     {slides.map((slide, index) => (
-                        <img key={index} src={slide} alt="slide" className="embla__slide" />
+                        <img key={index} src={slide} alt="slide" className="embla__slide aspect-square overflow-hidden" />
                         // <div className="embla__slide" key={index}>
                         //     <div className="embla__slide__number">{index + 1}</div>
                         // </div>
