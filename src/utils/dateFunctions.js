472af2af6899708b14bba22fa402ddf15176199d
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-23 20:09:46 -0300

    refactor: Update getAge function to return age's value more readable

--- src/utils/dateFunctions.js
+++ src/utils/dateFunctions.js
@@ -1,27 +1,29 @@
-//funcion para obtener la edad de la mascota
 export const getAge = (pet) => {
     let age;
     if (pet.age === undefined && pet.birthDate !== undefined) {
         const birthDate = new Date(pet.birthDate);
         const today = new Date();
         const diffTime = Math.abs(today - birthDate);
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
         const diffYears = Math.floor(diffDays / 365);
         const diffMonths = Math.floor((diffDays - diffYears * 365) / 30);
-        age = {
-            number: diffYears,
-            unit: diffYears > 1 ? "years" : "year",
-        };
+
         if (diffYears === 0) {
+            if (diffMonths === 0) {
+                age = {
+                    number: diffDays,
+                    unit: diffDays > 1 ? "días" : "día",
+                };
+            } else {
+                age = {
+                    number: diffMonths,
+                    unit: diffMonths > 1 ? "meses" : "mes",
+                };
+            }
+        } else {
             age = {
-                number: diffMonths,
-                unit: diffMonths > 1 ? "months" : "month",
-            };
-        }
-        if (diffMonths === 0) {
-            age = {
-                number: diffDays,
-                unit: diffDays > 1 ? "days" : "day",
+                number: diffYears,
+                unit: diffYears > 1 ? "años" : "año",
             };
         }
     }
