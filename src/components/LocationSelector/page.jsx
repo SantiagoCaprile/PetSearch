7b5abf7a3881287a9079b949a7552ac2fa4cc9f7
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-24 14:55:13 -0300

    hotFix: Update default location value in LocationSelector component

--- src/components/LocationSelector/page.jsx
+++ src/components/LocationSelector/page.jsx
@@ -7,12 +7,11 @@ import { useEffect } from "react";
 export default function LocationSelector() {
     useEffect(() => {
         if (localStorage.getItem("location") === null) {
-            localStorage.setItem("location", "CABA");
+            localStorage.setItem("location", "Buenos Aires");
         } else {
             document.getElementById("location").value = localStorage.getItem("location");
         }
-    }
-        , []);
+    }, []);
 
     return (
         <div className="flex items-center justify-center px-2">
@@ -21,6 +20,7 @@ export default function LocationSelector() {
                 className="text-white bg-inherit bg-opacity-55 rounded-md p-2 focus:outline-none w-4 md:w-60"
                 name="location"
                 id="location"
+                defaultValue={localStorage.getItem("location") || "Buenos Aires"}
                 onChange={(e) => {
                     localStorage.setItem("location", e.target.value);
                 }}
