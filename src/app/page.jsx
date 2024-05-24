f569a7bb83ffc14e1b39d8dd507740a2765a5b5b
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-23 21:11:42 -0300

    refactor: Remove unused Lucide-React components and change the access to pets page for desktop devices

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import PetsRandom from "../components/PetsRandom/page";
-import { Search, Store, Dog, Cat, MapPinned } from "lucide-react";
+import { Store, Dog, Cat, MapPinned } from "lucide-react";
 
 export const metadata = {
   title: "PetSearch",
@@ -19,34 +19,21 @@ export default async function HomePage() {
           </div>
           <div>
             <div className="flex flex-col md:flex-row justify-evenly items-center p-3 text-base rounded-t-md from-transparent to-green-900 bg-gradient-to-b">
-              <form className="hidden justify-center items-center md:flex">
-                <fieldset className="flex flex-col text-center text-2xl m-3 w-140">
-                  <label className="">¿Cual te gustaría adoptar?</label>
-                  <select className="text-center bg-slate-200 rounded-md text-black hover:bg-slate-300 transition-colors h-10 shadow-md border-2 border-slate-500">
-                    <option value="cualquiera">Cualquiera</option>
-                    <option value="perro">Perro</option>
-                    <option value="gato">Gato</option>
-                  </select>
-                </fieldset>
-                <fieldset className="flex flex-col text-center text-2xl m-3">
-                  <label>¿En donde te encontrás?</label>
-                  <select className="text-center bg-slate-200 rounded-md text-black hover:bg-slate-300 transition-colors h-10 shadow-md border-2 border-slate-500">
-                    <option value="CABA">CABA</option>
-                    <option value="Parana">Paraná</option>
-                    <option value="Santa Fe">Santa Fe</option>
-                    <option value="Rosario">Rosario</option>
-                  </select>
-                </fieldset>
+              <div className="flex md:flex-row justify-center items-center gap-1 flex-col">
                 <Link href="/pets">
                   <button className="flex gap-1 items-center bg-green-950 text-white border-2 border-green-500 px-16 py-2 rounded-full text-xl hover:bg-green-700 transition-all active:translate-y-1">
-                    <Search />
-                    Buscar
+                    <Dog />
+                    Perros
                   </button>
                 </Link>
-              </form>
-              <div className="flex md:flex-row justify-center items-center gap-1 flex-col-reverse">
-                <Link href="/helpMap">
+                <Link href="/pets">
                   <button className="flex gap-1 items-center bg-green-950 text-white border-2 border-green-500 px-16 py-2 rounded-full text-xl hover:bg-green-700 transition-all active:translate-y-1">
+                    <Cat />
+                    Gatos
+                  </button>
+                </Link>
+                <Link href="/helpMap">
+                  <button className="flex gap-1 items-center bg-green-950 text-white border-2 border-green-500 px-16 py-2 rounded-full text-xl hover:bg-green-700 transition-all active:translate-y-1 text-nowrap">
                     <MapPinned />
                     Pet GPS
                   </button>
@@ -55,18 +42,6 @@ export default async function HomePage() {
                   <button className="flex gap-1 items-center bg-green-950 text-white border-2 border-green-500 px-16 py-2 rounded-full text-xl hover:bg-green-700 transition-all active:translate-y-1">
                     <Store />
                     Rescatistas
-                  </button>
-                </Link>
-                <Link href="/pets">
-                  <button className="flex md:hidden gap-1 items-center bg-green-950 text-white border-2 border-green-500 px-16 py-2 rounded-full text-xl hover:bg-green-700 transition-all active:translate-y-1">
-                    <Dog />
-                    Perros
-                  </button>
-                </Link>
-                <Link href="/pets">
-                  <button className="flex md:hidden gap-1 items-center bg-green-950 text-white border-2 border-green-500 px-16 py-2 rounded-full text-xl hover:bg-green-700 transition-all active:translate-y-1">
-                    <Cat />
-                    Gatos
                   </button>
                 </Link>
               </div>
