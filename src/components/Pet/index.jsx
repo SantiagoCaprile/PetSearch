340eb444625775127972ea43039e72744433eb16
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-25 10:54:55 -0300

    refactor: Update Pet component to include hover effect and shadow

--- src/components/Pet/index.jsx
+++ src/components/Pet/index.jsx
@@ -6,7 +6,7 @@ export default function Pet({ pet }) {
   let age = getAge(pet);
   return (
     <div key={pet._id} className="group relative">
-      <div className="min-h-5 aspect-video w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-52">
+      <div className="min-h-5 aspect-video w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:scale-105 transition-all lg:h-52 shadow-sm">
         {
           pet.images && pet.images.length > 0 &&
           <Image
