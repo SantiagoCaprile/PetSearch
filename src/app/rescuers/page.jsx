9c2c298a46115b8b88ddfe3175771f2afa768f61
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-26 19:30:15 -0300

    feat: Update Rescuers and RescuerCard to fetch rescuers from database

--- src/app/rescuers/page.jsx
+++ src/app/rescuers/page.jsx
@@ -2,19 +2,22 @@
 import React from "react";
 import RescuerCard from "@components/RescuerCard/page";
 import RescuersFilters from "@components/RescuersFilters/page";
-
-const rescuers = []
-for (let i = 1; i < 9; i++) {
-  rescuers.push({
-    id: i,
-    name: `Rescatista ${i}`,
-    location: `Rosario`,
-    telephone: `342-123456${i}`,
-    image: "/images/rescuerProfile.svg",
-  });
-}
+import { useEffect, useState } from "react";
+import Rescuer from "@/classes/Rescuer";
 
 export default function RescuersPage() {
+  const [rescuers, setRescuers] = useState([]);
+  useEffect(() => {
+    Rescuer.getAll()
+      .then((res) => {
+        setRescuers(res.rescuers);
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="flex flex-col flex-1 justify-center items-center">
       <div className="shadow-md rounded p-4 md:p-8 mb-4 max-w-[1200px] w-4/5 bg-white mt-4">
@@ -23,8 +26,9 @@ export default function RescuersPage() {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {
+          rescuers &&
           rescuers.map((rescuer) => (
-            <RescuerCard key={rescuer.id} rescuer={rescuer} />
+            <RescuerCard key={rescuer._id} rescuer={rescuer} />
           ))
         }
       </div>
--- src/components/RescuerCard/page.jsx
+++ src/components/RescuerCard/page.jsx
@@ -5,8 +5,9 @@ export default function RescuerCard({ rescuer }) {
     if (!rescuer) {
         return null;
     }
+
     return (
-        <Link href={`/rescuers/${rescuer.id}`}>
+        <Link href={`/rescuers/${rescuer.user._id}`}>
             <div className="flex flex-col items-center justify-center bg-white rounded-lg shadow-lg p-4 m-4 w-[300px] hover:translate-y-1 transition-all cursor-pointer"
             >
                 <Image
@@ -17,9 +18,9 @@ export default function RescuerCard({ rescuer }) {
                     width={150}
                     height={150}
                 />
-                <h2 className="text-xl font-bold mt-4">{rescuer.name}</h2>
-                <p className="text-gray-500 text-sm">{rescuer.location}</p>
-                <p className="text-gray-500 text-sm">{rescuer.telephone}</p>
+                <h2 className="text-xl font-bold mt-4">{rescuer.user.name}</h2>
+                <p className="text-gray-500 text-sm">{rescuer.city}</p>
+                <p className="text-gray-500 text-sm">{rescuer.contactPhone}</p>
             </div>
         </Link>
     );
