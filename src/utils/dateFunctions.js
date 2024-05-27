20beeb54637054d8a2cfdf2b9c6a04460030fae3
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-27 17:10:54 -0300

    feat: Add formatDateToDDMMYYYY function to dateFunctions.js

--- src/utils/dateFunctions.js
+++ src/utils/dateFunctions.js
@@ -29,3 +29,12 @@ export const getAge = (pet) => {
     }
     return age;
 };
+
+export function formatDateToDDMMYYYY(isoDate) {
+    const date = new Date(isoDate);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
