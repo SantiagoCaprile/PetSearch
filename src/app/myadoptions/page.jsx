96727ba37f21733fb1d4aa4f908ff910c7466e0b
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-27 18:52:05 -0300

    feat: Improve MyAdoptions page loading and error handling

--- src/app/myadoptions/page.jsx
+++ src/app/myadoptions/page.jsx
@@ -4,32 +4,42 @@ import AdoptionCard from "@/components/AdoptionCard/page";
 import Adoption from "@/classes/Adoption";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import Loader from "@/components/Loader";
 
 export default function MyAdoptions() {
     const { data: session } = useSession();
     const id = session?.user?._id;
-    const [adoptions, setAdoptions] = useState([]);
+    const [adoptions, setAdoptions] = useState('loading');
 
     useEffect(() => {
         const response = Adoption.getAdoptionsForRescuer(id)
             .then((response) => {
                 setAdoptions(response);
-                console.log("Adoptions for user:", response);
             }
             ).catch((error) => {
+                setAdoptions('error');
                 console.error("An error occurred:", error);
             });
         if (!response) {
-            console.log("Failed to get adoptions for user");
+            console.log("Failed to get adoptions");
         }
     }
         , [id]);
 
+    if (adoptions === 'error') {
+        return <div className="text-center text-red-500">
+            Error al cargar las adopciones
+            <br />
+            Intentelo mas tarde
+        </div>;
+    }
+
     return (
         <div className="grid grid-cols-1 place-items-center gap-4 align-middle md:grid-cols-2 lg:grid-cols-3 my-4">
-            {adoptions && adoptions.map((adoption) => (
-                <AdoptionCard key={adoption.pet._id} adoption={adoption} pet={adoption.pet} user={adoption.user} />
-            ))}
+            {adoptions === 'loading' ? <div className=" col-span-3"><Loader /></div>
+                : adoptions?.map((adoption) => (
+                    <AdoptionCard key={adoption.pet._id} adoption={adoption} pet={adoption.pet} user={adoption.user} />
+                ))}
         </div>
     );
 }
