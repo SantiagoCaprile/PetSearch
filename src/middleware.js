c9c3b152ef517af0a8739291d16df82740e3d6ac
Santi Caprile <santiagocaprile7@gmail.com>
2024-05-27 16:27:55 -0300

    chore: Update middleware to restrict access to createPet and adoptionForm pages based on user role

--- src/middleware.js
+++ src/middleware.js
@@ -4,20 +4,19 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
-    // if (
-    //   req.nextUrl.pathname.startsWith("/createPet") &&
-    //   req.nextauth.token.role !== "rescuer"
-    // ) {
-    //   console.log("no es rescuer, no puede crear mascotas");
-    //   return NextResponse.rewrite(new URL("/denied", req.url));
-    // }
-
     if (
-      req.nextUrl.pathname.startsWith("/adoptionform") &&
+      req.nextUrl.pathname.startsWith("/createPet") &&
+      req.nextauth.token.role !== "rescuer"
+    ) {
+      console.log("no es rescuer, no puede crear mascotas");
+      return NextResponse.rewrite(new URL("/denied", req.url));
+    }
+    if (
+      req.nextUrl.pathname.startsWith("/adoptionform/") &&
       req.nextauth.token.role !== "user"
     ) {
-      console.log("no es user");
-      return NextResponse.rewrite(new URL("/api/auth/signin", req.url));
+      console.log("no es user, no puede adoptar mascotas");
+      return NextResponse.rewrite(new URL("/denied", req.url));
     }
   },
   {
@@ -32,5 +31,5 @@ export default withAuth(
 
 // Applies next-auth only to matching routes - can be regex
 export const config = {
-  matcher: ["/profile", "/createPet", "/adoptionform"],
+  matcher: ["/profile", "/createPet", "/adoptionform/:petId/:path*"]
 };
