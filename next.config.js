6f1d052d7ef252cee5cd3b86c4ae4e5aaf621737
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-12 12:27:53 -0300

    chore: Migrate to Next 14. Update dependencies and add /static folder to gitignore file

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   env: {
     API_URL: process.env.API_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     API_KEY: process.env.API_KEY,
   },
   images: {
-    domains: [
-      "i.redd.it",
-      "firebasestorage.googleapis.com",
-      "www.purina.com.ar",
-      "lh3.googleusercontent.com",
-      "res.cloudinary.com",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/dprm5aerx/**',
+      },
     ],
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './components/**/*.{js,jsx}',
     './app/**/*.{js,jsx}',
     './src/**/*.{js,jsx}',
-	],
+  ],
   theme: {
     container: {
       center: true,
