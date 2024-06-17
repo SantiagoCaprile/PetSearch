f62a9c3ca17c9f1845fdb8f2146af6b32d5d11cc
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-17 11:07:30 -0300

    feat: Optimize image editing, uploading and conversion process on CreatePetForm

--- src/components/CreatePetForm/page.jsx
+++ src/components/CreatePetForm/page.jsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 import { toast } from 'react-hot-toast';
 import Pet from "@/classes/Pet";
 import CalendarComponent from "@components/Calendar/page.jsx"
+import { X, Upload, RefreshCcw } from "lucide-react";
+import { convertBatchImagesToBase64 } from "@/utils/imgFunctions";
 
 //This is a component that allows the user to create a pet o edit.
 // it depends if the pet object is passed as a prop or not.
@@ -22,6 +24,7 @@ export default function CreatePetForm({ editPet }) {
 
     const [images, setImages] = useState([]);
     const [imagePreviews, setImagePreviews] = useState([]);
+    const [imgSaved, setImgSaved] = useState(false);
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
@@ -47,8 +50,9 @@ export default function CreatePetForm({ editPet }) {
                     setValue("neutered", pet.characteristics.find(c => c.key === "neutered").value);
                     setValue("vaccinated", pet.characteristics.find(c => c.key === "vaccinated").value);
                     setDate(new Date(pet.birthDate));
-                    setImagePreviews(pet.images);
-                    // setImages(pet.images); VER COMO IMPLIEMENTAR ESTO EN EL EDIT
+                    setImagePreviews(pet.images); //this is to show the images
+                    setImages(pet.images); //this is to transform the images to base64 to save
+                    setImgSaved(pet.images);//this is to refresh in case a mistake
                 }
             }).catch((error) => {
                 console.error(error);
@@ -58,6 +62,8 @@ export default function CreatePetForm({ editPet }) {
 
     //should optimize the images
     const handleImagesChange = async (e) => {
+        // if there are already images, should not overwrite them
+        // should check if imagePreviews.lenght + files.length > 3
         const files = Array.from(e.target.files);
         if (files.length > 3) {
             toast.error("No se pueden subir más de 3 imágenes");
@@ -65,44 +71,35 @@ export default function CreatePetForm({ editPet }) {
             e.target.value = null;
             return;
         }
-        const imagePreviews = files.map(file => URL.createObjectURL(file));
-        setImagePreviews(imagePreviews);
-        const base64Images = await Promise.all(files.map(file => {
-            return new Promise((resolve, reject) => {
-                const reader = new FileReader();
-                reader.readAsDataURL(file);
-                reader.onload = () => {
-                    const img = new Image();
-                    img.src = reader.result;
-                    img.onload = () => {
-                        const canvas = document.createElement('canvas');
-                        const MAX_WIDTH = 1280;
-                        const MAX_HEIGHT = 720;
-                        let width = img.width;
-                        let height = img.height;
-                        if (width > height) {
-                            if (width > MAX_WIDTH) {
-                                height *= MAX_WIDTH / width;
-                                width = MAX_WIDTH;
-                            }
-                        } else {
-                            if (height > MAX_HEIGHT) {
-                                width *= MAX_HEIGHT / height;
-                                height = MAX_HEIGHT;
-                            }
-                        }
-                        canvas.width = width;
-                        canvas.height = height;
-                        const ctx = canvas.getContext('2d');
-                        ctx.drawImage(img, 0, 0, width, height);
-                        const optimizedImage = canvas.toDataURL('image/jpeg', 0.8);
-                        resolve(optimizedImage);
-                    };
-                };
-                reader.onerror = error => reject(error);
-            });
-        }));
-        setImages(base64Images);
+
+        if (editPet && (imagePreviews.length + files.length > 3)) {
+            toast.error("No se pueden subir más de 3 imágenes");
+            setImagePreviews(imgSaved);
+            e.target.value = null;
+            return;
+        }
+
+        const imagePreviewUrl = files.map(file => URL.createObjectURL(file));
+        setImagePreviews([...imagePreviews, ...imagePreviewUrl]);
+
+        const base64Images = await convertBatchImagesToBase64(files);
+
+        //check if the image already exists
+        // const newImages = base64Images.filter(image => !wasImageAlreadyUploaded(images, image));
+        const newImgs = base64Images.filter((image, index) => {
+            if (wasImageAlreadyUploaded(images, image)) {
+                // setImagePreviews(imagePreviews.splice(index, 1));
+                // setImages(images.splice(index, 1));
+                //show a toast with a warning card
+                toast.error("Parece haber imagenes duplicadas", { icon: "⚠️" })
+                return null;
+            }
+            return image;
+        });
+
+        setImages([...images, ...base64Images]);
+
+        e.target.value = null;
     };
 
     const onSubmit = async (data) => {
@@ -137,13 +134,18 @@ export default function CreatePetForm({ editPet }) {
         };
         editPet ? pet._id = editPet : null;
         if (editPet) {
-            submitEditPet(pet, session).finally(() => {
-                router.push("/mypets");
-            });
+            submitEditPet(pet, session)
+                .then(() => {
+                    router.push("/mypets");
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
         } else {
-            submitCreatePet(pet, session).finally(() => {
-                router.push("/mypets");
-            });
+            submitCreatePet(pet, session)
+                .finally(() => {
+                    router.push("/mypets");
+                });
         }
     };
 
@@ -316,34 +318,65 @@ export default function CreatePetForm({ editPet }) {
                         />
                     </label>
                 </div>
-                <div className="mb-4">
-                    <label htmlFor="images" className="block text-lg font-semibold">
+                <div className="mb-4 flex flex-col">
+                    <label htmlFor="images" className="text-lg font-semibold">
                         Imagenes
+                    </label>
+                    <label htmlFor="images" className="text-center p-6 rounded-lg border-0 text-sm font-semibold bg-green-200 text-green-700 cursor-pointer transition duration-150 ease-in-out 
+                        transform hover:translate-y-1 hover:shadow-md active:translate-y-2 active:shadow-lg hover:bg-green-300 md:max-w-lg mb-2">
+                        <Upload size={24} className="mr-2 inline" />
+                        <span>
+                            Subir archivos aqui
+                        </span>
                     </label>
                     <input
                         type="file"
                         id="images"
+                        accept="image/*"
+                        title="Seleccionar imagenes"
                         multiple
                         onChange={handleImagesChange}
-                        className="file:mr-4 file:p-8
-      file:rounded-lg file:border-0
-      file:text-sm file:font-semibold
-      file:bg-green-200 file:text-green-700
-      file:cursor-pointer file:transition file:duration-150 file:ease-in-out file:transform file:hover:scale-105
-      hover:file:bg-green-300 md:max-w-lg"
+                        className="hidden"
                     />
                 </div>
+                {editPet && imagePreviews.length !== 0 &&
+                    < button
+                        className="bg-blue-500 text-white px-4 py-2 rounded-md mb-2"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setImagePreviews(imgSaved);
+                            setImages(imgSaved);
+                        }}
+                    >
+                        <RefreshCcw size={20} className="mr-2 inline" />
+                        <span>
+                            Restaurar Imagenes
+                        </span>
+                    </button>
+                }
                 <div className="image-preview flex gap-1 h-fit mb-2">
-                    {imagePreviews.map((image, index) => (
-                        <div key={index} className="image-container w-24 md:w-36 overflow-hidden">
-                            <img src={image} alt={`Preview ${index}`} className="thumbnail" />
-                        </div>
-                    ))}
+                    {
+                        imagePreviews.map((image, index) => (
+                            <ImagePreview
+                                key={index}
+                                image={image}
+                                index={index}
+                                handleDelete={(index) => {
+                                    const newImages = [...images];
+                                    newImages.splice(index, 1);
+                                    setImages(newImages);
+                                    const newImagePreviews = [...imagePreviews];
+                                    newImagePreviews.splice(index, 1);
+                                    setImagePreviews(newImagePreviews);
+                                }}
+                            />
+                        ))
+                    }
                 </div>
                 <button
                     id="submitButton"
                     type="submit"
-                    className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                    className="bg-green-500 text-white px-4 py-2 rounded-md transition duration-150 ease-in-out transform hover:scale-105"
                 >
                     {editPet ? "Guardar Mascota" : "Crear Mascota"}
                 </button>
@@ -354,33 +387,65 @@ export default function CreatePetForm({ editPet }) {
 
 //this will be used to create a pet
 async function submitCreatePet(pet, session) {
-    const toastId = toast.loading("Creando mascota...");
-    try {
-        const response = await Pet.createPet(pet, { token: session.jwtApiToken, role: session.user.role });
-        if (response) {
-            toast.success("Mascota creada exitosamente", { id: toastId });
-        } else {
-            throw new Error("Failed to create pet");
+    return new Promise(async (resolve, reject) => {
+        const toastId = toast.loading("Creando mascota...");
+        try {
+            const response = await Pet.createPet(pet, { token: session.jwtApiToken, role: session.user.role });
+            if (response) {
+                toast.success("Mascota creada exitosamente", { id: toastId });
+                resolve(response);
+            } else {
+                throw new Error("Failed to create pet");
+            }
+        } catch (error) {
+            toast.error("Error al crear mascota", { id: toastId });
+            document.getElementById("submitButton").disabled = false;
+            reject(error);
         }
-    } catch (error) {
-        toast.error("Error al crear mascota", { id: toastId });
-        document.getElementById("submitButton").disabled = false;
-    }
+    });
 }
 
 //this will be used to edit a pet
 async function submitEditPet(pet, session) {
-    const toastId = toast.loading("Guardando mascota...");
-    try {
-        const response = await Pet.updatePet(pet._id, pet, { token: session.jwtApiToken, role: session.user.role });
-        if (response) {
-            toast.success("Mascota editada exitosamente", { id: toastId });
-        } else {
-            throw new Error("Failed to edit pet");
+    return new Promise(async (resolve, reject) => {
+        const toastId = toast.loading("Guardando mascota...");
+        try {
+            const response = await Pet.updatePet(pet._id, pet, { token: session.jwtApiToken, role: session.user.role });
+            if (response) {
+                toast.success("Mascota editada exitosamente", { id: toastId });
+                resolve(response);
+            } else {
+                throw new Error("Failed to edit pet");
+            }
+        } catch (error) {
+            toast.error("Error al editar mascota", { id: toastId });
+            document.getElementById("submitButton").disabled = false;
+            reject(error);
         }
-    } catch (error) {
-        toast.error("Error al editar mascota", { id: toastId });
-        document.getElementById("submitButton").disabled = false;
-    }
+    });
 }
 
+//this will be a component that will allow the user preview the image and delete it if doesn't like it
+function ImagePreview({ image, index, handleDelete }) {
+    return (
+        <div className="w-24 md:w-36 overflow-hidden relative">
+            <button
+                onClick={(e) => {
+                    e.preventDefault();
+                    handleDelete(index)
+                }}
+                className="bg-red-500 text-white p-1 rounded-sm absolute top-0 right-0 z-10
+                transition duration-150 ease-in-out transform hover:shadow-md active:translate-x-1 active:shadow-lg hover:bg-red-600"
+            >
+                < X size={20} aria-label="Eliminar" />
+                <span className="sr-only">Eliminar</span>
+            </button>
+            <img src={image} alt={`Preview ${index}`} className="thumbnail rounded-sm" />
+        </div >
+    );
+}
+
+//this will be a function to check if a user is not uploading the same image twice
+function wasImageAlreadyUploaded(images, image) {
+    return images.some(img => img === image);
+}
--- src/utils/imgFunctions.js
+++ src/utils/imgFunctions.js
@@ -1,36 +1,10 @@
+// now we make the optimization of the image on the backend
 export async function convertBatchImagesToBase64(files) {
     const base64Images = await Promise.all(files.map(file => {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
             reader.readAsDataURL(file);
-            reader.onload = () => {
-                const img = new Image();
-                img.src = reader.result;
-                img.onload = () => {
-                    const canvas = document.createElement('canvas');
-                    const MAX_WIDTH = 1280;
-                    const MAX_HEIGHT = 720;
-                    let width = img.width;
-                    let height = img.height;
-                    if (width > height) {
-                        if (width > MAX_WIDTH) {
-                            height *= MAX_WIDTH / width;
-                            width = MAX_WIDTH;
-                        }
-                    } else {
-                        if (height > MAX_HEIGHT) {
-                            width *= MAX_HEIGHT / height;
-                            height = MAX_HEIGHT;
-                        }
-                    }
-                    canvas.width = width;
-                    canvas.height = height;
-                    const ctx = canvas.getContext('2d');
-                    ctx.drawImage(img, 0, 0, width, height);
-                    const optimizedImage = canvas.toDataURL('image/jpeg', 0.8);
-                    resolve(optimizedImage);
-                };
-            };
+            reader.onload = () => resolve(reader.result);
             reader.onerror = error => reject(error);
         });
     }));
