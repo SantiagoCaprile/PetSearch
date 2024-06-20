52df8b868660d18b02994bf1f870effa081ebeee
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-20 15:18:30 -0300

    refactor: Update AdminDashboard page to include navigation links for different sections

--- src/app/adminDashboard/page.jsx
+++ src/app/adminDashboard/page.jsx
@@ -1,16 +1,43 @@
 'use client';
 import React from "react";
-import AdminLocations from "@/components/AdminComponents/AdminLocations/page";
+import Link from 'next/link';
 //this page will be the admin dashboard
 //will have many options for the admin to manage the app
 //divided in tabs
 
+const routes = [
+    {
+        name: "Locations",
+        url: "/adminDashboard/locations",
+    },
+    {
+        name: "Pets",
+        url: "/adminDashboard/pets",
+    },
+    {
+        name: "Users",
+        url: "/adminDashboard/users",
+    },
+    {
+        name: "Rescuers",
+        url: "/adminDashboard/rescuers",
+    },
+];
+
 export default function AdminDashboard() {
     return (
-        <div className="flex flex-col gap-4">
-            <h1 className="text-2xl font-bold">Dashboard</h1>
-            <AdminLocations />
-        </div>
+        <div className="flex flex-1 bg-slate-50 items-center flex-col gap-4">
+            <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+            <div className="flex flex-col gap-4">
+                {routes.map((route, index) => (
+                    <Link key={index} href={route.url} className={(index % 2 === 0 ? "bg-blue-400 " : "bg-green-400 "
+                    ) + buttonsStyles}>
+                        {route.name}
+                    </Link>
+                ))}
+            </div>
+        </div >
     );
 }
 
+const buttonsStyles = "text-xl text-center font-semibold text-white font-semibold rounded-md p-8 hover:bg-slate-600 transition-all duration-200"
