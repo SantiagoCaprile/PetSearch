630bcd27a5e4bef260cdb91f2ba140e2633613af
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-22 03:53:10 -0300

    refactor: Add Metadata component for managing page metadata

---
+++ src/components/Metadata/page.jsx
@@ -0,0 +1,21 @@
+// components/Metadata.js
+const Metadata = ({ title, description, keywords, openGraph, twitter }) => (
+    <div hidden>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords.join(", ")} />
+        <meta property="og:title" content={openGraph?.title} />
+        <meta property="og:description" content={openGraph?.description} />
+        <meta property="og:image" content={openGraph?.image} />
+        <meta property="og:url" content={openGraph?.url} />
+        <meta property="og:type" content={openGraph?.type} />
+        <meta name="twitter:card" content={twitter.card} />
+        <meta name="twitter:site" content={twitter.site} />
+        <meta name="twitter:creator" content={twitter.creator} />
+        <meta name="twitter:title" content={twitter.title} />
+        <meta name="twitter:description" content={twitter.description} />
+        <meta name="twitter:image" content={twitter.image} />
+    </div>
+);
+
+export default Metadata;
---
+++ src/utils/metadata.js
@@ -0,0 +1,23 @@
+// utils/metadata.js
+export const defaultMetadata = {
+    metadataBase: new URL("https://www.petsearch.com.ar"),
+    keywords: ["PetSearch", "Mascotas", "Adopciones", "Rescatistas", "Argentina",],
+    title: {
+        default: "PetSearch",
+        end: " - PetSearch",
+    },
+    openGraph: {
+        description: "PetSearch es una aplicación para encontrar tu mascota ideal",
+        image: "/images/thumbnail.webp",
+        title: "PetSearch",
+    },
+    twitter: {
+        card: "summary_large_image",
+        site: "@PetSearch",
+        creator: "@PetSearch",
+        title: "PetSearch",
+        description: "PetSearch es una aplicación para encontrar tu mascota ideal",
+        image: "/images/thumbnail.webp",
+    }
+};
+
