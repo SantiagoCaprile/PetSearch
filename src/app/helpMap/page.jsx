f3fd18d26efa9f1cd3c62af9d69ddd8937400c40
Santi Caprile <santiagocaprile7@gmail.com>
2024-06-25 11:30:07 -0300

    fix: Modify helpMap page to update lostAndFoundPets list correctly

--- src/app/helpMap/page.jsx
+++ src/app/helpMap/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import Map from "@/components/Map";
 import Link from "next/link";
@@ -11,47 +11,37 @@ import { defaultMetadata } from "@/utils/metadata";
 
 export default function HelpMap() {
     const locations = useSelector((state) => state.location);
-    const [city, setCity] = useState(null)
-    const [map, setMap] = useState(false)
-    const [lostAndFoundPets, setLostAndFoundPets] = useState([])
+    const [city, setCity] = useState(null);
+    const [map, setMap] = useState(false);
+    const [lostAndFoundPets, setLostAndFoundPets] = useState([]);
 
     useEffect(() => {
-        if (typeof window !== "undefined") {
-            window.localStorage.getItem("location")
-                && (locations ? locations.location : window.localStorage.getItem("location"))
-            setMap(true)
-            HelpFormClass.getHelpFormByCity(locations.location.location?.name)
-                .then((data) => {
-                    setLostAndFoundPets(data)
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-        }
-        handleSetCity()
-    }, [locations.location])
-
-    const handleSetCity = (e) => {
-        setMap(false)
-        setCity(locations.location.location) //redux slice.location object.city
-        city &&
-            HelpFormClass.getHelpFormByCity(city.name)
-                .then((data) => {
-                    setLostAndFoundPets(data)
-                })
-                .catch((err) => {
-                    console.log(err)
+        const fetchData = async () => {
+            if (locations.location.location) {
+                try {
+                    const data = await HelpFormClass.getHelpFormByCity(locations.location.location.name);
+                    setLostAndFoundPets(data);
+                    setMap(false);
+                    setCity(locations.location.location);
+                    setTimeout(() => {
+                        setMap(true);
+                    }, 200);
+                } catch (err) {
+                    console.error(err);
                 }
-                )
-        setTimeout(() => {
-            setMap(true)
-        }, 200)
-    }
+            } else {
+                setCity(null);
+                setLostAndFoundPets([]);
+                setMap(false);
+            }
+        };
+
+        fetchData();
+    }, [locations, locations]);
 
     const metadata = {
         ...defaultMetadata,
-        title: "Mapa de Mascotas Perdidas y Encontradas en Argentina"
-            + defaultMetadata.separator + defaultMetadata.title,
+        title: "Mapa de Mascotas Perdidas y Encontradas en Argentina" + defaultMetadata.separator + defaultMetadata.title,
         description: "Mapa de mascotas perdidas y encontradas en Argentina. Publica un anuncio si perdiste o encontraste un animalito",
     };
 
@@ -81,79 +71,71 @@ export default function HelpMap() {
                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                                 />
-                                {lostAndFoundPets && lostAndFoundPets.length > 0 &&
-                                    lostAndFoundPets.map((pet, index) => (
-                                        <Marker
-                                            key={index}
-                                            position={[pet.location.lat, pet.location.lng]}
-                                            icon={L.icon({
-                                                iconUrl: pet.type == HelpFormClass.TYPE.LOST ? "/leaflet/images/marker-icon-red.png" : "/leaflet/images/marker-icon-green.png",
-                                                popupAnchor: [0, -30],
-                                                iconSize: [20, 30],
-                                                shadowUrl: "/leaflet/images/marker-shadow.png",
-                                                iconAnchor: [10, 30],
-                                                shadowSize: [20, 30],
-                                            })}
-                                        >
-                                            <Popup>
-                                                <PopupData pet={pet} />
-                                            </Popup>
-                                        </Marker>))
-                                }
+                                {lostAndFoundPets.length > 0 && lostAndFoundPets.map((pet, index) => (
+                                    <Marker
+                                        key={index}
+                                        position={[pet.location.lat, pet.location.lng]}
+                                        icon={L.icon({
+                                            iconUrl: pet.type === HelpFormClass.TYPE.LOST ? "/leaflet/images/marker-icon-red.png" : "/leaflet/images/marker-icon-green.png",
+                                            popupAnchor: [0, -30],
+                                            iconSize: [20, 30],
+                                            shadowUrl: "/leaflet/images/marker-shadow.png",
+                                            iconAnchor: [10, 30],
+                                            shadowSize: [20, 30],
+                                        })}
+                                    >
+                                        <Popup>
+                                            <PopupData pet={pet} />
+                                        </Popup>
+                                    </Marker>
+                                ))}
                             </>
                         )}
                     </Map>
                 ) : (
-                    locations &&
-                        !locations.loading ?
+                    locations && !locations.loading ? (
                         <div className="flex justify-center items-center bg-slate-400 animate-pulse rounded-lg h-full">
                             <h2 className="text-2xl font-bold text-white">Cargando Mapa...</h2>
                         </div>
-                        :
+                    ) : (
                         <div className="flex justify-center items-center bg-slate-400 rounded-lg h-full">
                             <h2 className="text-2xl font-bold text-white">Selecciona tu ciudad en la barra superior</h2>
                         </div>
+                    )
                 )}
             </div>
-        </div >
-    )
+        </div>
+    );
 }
 
 // this will be the component for the popup to display the data of the pet
 import Image from "next/image";
 function PopupData({ pet }) {
-    const [showMore, setShowMore] = useState(false)
+    const [showMore, setShowMore] = useState(false);
 
     const handleShowMore = () => {
-        setShowMore(!showMore)
-    }
+        setShowMore(!showMore);
+    };
 
     return (
         <div className={"w-36 flex-col items-center justify-center max-h-80 overflow-scroll"}>
             <Image src={pet.image} alt="dog" className="aspect-square object-cover overflow-hidden rounded-md" width={200} height={200} />
-            <div className={"text-center rounded-lg " + (pet.type == HelpFormClass.TYPE.LOST ? " bg-red-200" : "bg-green-100")}>
+            <div className={"text-center rounded-lg " + (pet.type === HelpFormClass.TYPE.LOST ? " bg-red-200" : "bg-green-100")}>
                 <p>
-                    <b className="text-lg">{pet.type == HelpFormClass.TYPE.LOST ? "Perdido " : "Encontrado "}</b>
+                    <b className="text-lg">{pet.type === HelpFormClass.TYPE.LOST ? "Perdido " : "Encontrado "}</b>
                     <br />
                     {formatDateToDDMMYYYY(pet.date)}
                     <br />
-                    {
-                        pet.description.length < 40 ? pet.description
-                            :
-                            !showMore ? (pet.description?.slice(0, 40) + "...") : pet.description
-                    }
-                    {
-                        pet.description.length > 40 &&
+                    {pet.description.length < 40 ? pet.description : (!showMore ? (pet.description?.slice(0, 40) + "...") : pet.description)}
+                    {pet.description.length > 40 && (
                         <span className="pl-2 text-blue-500 font-bold cursor-pointer text-nowrap" onClick={handleShowMore}>
                             {!showMore ? "Ver más" : "Menos"}
                         </span>
-                    }
+                    )}
                     <br />
-                    <span className="font-bold">
-                        {pet.cel}
-                    </span>
+                    <span className="font-bold">{pet.cel}</span>
                 </p>
             </div>
         </div>
-    )
+    );
 }
